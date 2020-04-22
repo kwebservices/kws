f95312808e3c4c5503ec9493f91e6a3ed16b83d0
David Plath <support@krealmwebservices.com.au>
2020-04-22 14:04:50 +1000

    Added various joomla view classes

---
+++ src/Joomla/View/Site/GenericSiteView.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * =============================================================================
+ * @package     KRealm Web Services PHP Library
+ * @author      David Plath <webmaster@krealmwebservices.com.au>
+ * @copyright   Copyright (C) 2020 KRealm Web Services. All rights reserved.
+ * @license     GNU General Public License version 3 or later
+ * =============================================================================
+ */
+
+namespace KWS\Joomla\View\Site;
+
+
+use \Joomla\CMS\MVC\View\HtmlView;
+
+
+/**
+ * Class for creating generic site/front-end view classes
+ */
+class GenericSiteView extends HtmlView
+{
+}
