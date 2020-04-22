de182c0c3ef19d30883f25c8f04f813252406f8d
David Plath <support@krealmwebservices.com.au>
2020-04-22 15:28:54 +1000

    Added a couple classes for FuelPHP

---
+++ src/FuelPHP/Orm/Model.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * =============================================================================
+ * @package     KRealm Web Services PHP Library
+ * @author      David Plath <webmaster@krealmwebservices.com.au>
+ * @copyright   Copyright (C) 2020 KRealm Web Services. All rights reserved.
+ * @license     GNU General Public License version 3 or later
+ * =============================================================================
+ */
+
+namespace KWS\FuelPHP\Orm;
+
+
+
+class Model extends \Orm\Model
+{
+
+
+    public function hydrateFromInput()
+    {
+        $properties = array_keys($this->properties());
+
+        foreach ($properties as $name) {
+
+            $value = \Input::param($name, null);
+
+            if (!is_null($value)) {
+                $this->set($name, $value);
+            }
+        }
+
+    }
+
+}
