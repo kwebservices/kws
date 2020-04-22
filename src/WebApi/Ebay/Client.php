5010b4f9eace73923ece400687f33856c57c011a
David Plath <support@krealmwebservices.com.au>
2020-04-22 15:18:54 +1000

    Added basic skeleton for working with various web apis

---
+++ src/WebApi/Ebay/Client.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * =============================================================================
+ * @package     KRealm Web Services PHP Library
+ * @author      David Plath <webmaster@krealmwebservices.com.au>
+ * @copyright   Copyright (C) 2020 KRealm Web Services. All rights reserved.
+ * @license     GNU General Public License version 3 or later
+ * =============================================================================
+ */
+
+namespace KWS\WebApi\Ebay;
+
+
+/**
+ * Class for working with the EBay API
+ */
+class Client
+{
+}
