1447376cbe56dce7b16f7a2c73ffd4c08ea33d0b
David Plath <support@krealmwebservices.com.au>
2020-04-22 14:40:43 +1000

    Removed ACMA related methods from utils class

--- src/Utils.php
+++ src/Utils.php
@@ -13,9 +13,6 @@ namespace KWS;
 
 use \Dompdf\Dompdf;
 use \ScssPhp\ScssPhp\Compiler AS SCSSCompiler;
-use \League\Flysystem\Filesystem;
-use \League\Flysystem\ZipArchive\ZipArchiveAdapter;
-use \League\Csv\Reader AS CsvReader;
 
 
 /**
@@ -643,37 +640,7 @@ class Utils
         // Return the result body
         return $result->getBody();
     }
-
-
-
-    /**
-     * Download, extract, parse and then return ACMA's register of numbers from
-     * thier website. This method may take allot of time to complete. Caching
-     * the result is recommended. ACMA updated the register of numbers once
-     * every day at midnight.
-     * -------------------------------------------------------------------------
-     * @return string   A very large array of records parsed from the data.
-     */
-    public static function downloadAcmaRegisterOfNumbers()
-    {
-        // Download the ZIP file and save it to a tempory location
-        $filename = tempnam(sys_get_temp_dir(), '');
-        $url = 'https://www.thenumberingsystem.com.au/download/EnhancedFullDownload.zip';
-        file_put_contents($filename, file_get_contents($url));
-
-        // Extract the CSV file which contains the registar of numbers
-        $zipReader = new Filesystem(new ZipArchiveAdapter($filename));
-        $result = $zipReader->read('EnhancedFullDownload.csv');
-
-        // Parse the CSV data
-        $csvReader = CsvReader::createFromString($result);
-        $csvReader->setHeaderOffset(0);
-        $result = iterator_to_array($csvReader->getRecords());
-
-        // Return the result
-        return $result;
-    }
-
+    
 
     /**
      * Get the HTML/Javascript for displaying a reCAPTCHA 3
