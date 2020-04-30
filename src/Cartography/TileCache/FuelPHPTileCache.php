f23fa9eb90c7e0bd0526c63bae9a9a866a7e2527
David Plath <support@krealmwebservices.com.au>
2020-04-30 14:57:06 +1000

    Added classes for caching map tiles with Sqlite3 and FuelPHP

---
+++ src/Cartography/TileCache/FuelPHPTileCache.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * =============================================================================
+ * @package     KRealm Web Services PHP Library
+ * @author      David Plath <webmaster@krealmwebservices.com.au>
+ * @copyright   Copyright (C) 2020 KRealm Web Services. All rights reserved.
+ * @license     GNU General Public License version 3 or later
+ * =============================================================================
+ */
+
+namespace KWS\Cartography\TileCache;
+
+
+/**
+ * Class for caching map tiles using FuelPHP's caching system.
+ */
+class FuelPHPTileCache extends TileCache
+{
+}
---
+++ src/Cartography/TileCache/SqLite3TileCache.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * =============================================================================
+ * @package     KRealm Web Services PHP Library
+ * @author      David Plath <webmaster@krealmwebservices.com.au>
+ * @copyright   Copyright (C) 2020 KRealm Web Services. All rights reserved.
+ * @license     GNU General Public License version 3 or later
+ * =============================================================================
+ */
+
+namespace KWS\Cartography\TileCache;
+
+
+/**
+ * Class for caching map tiles in an sqlite3 database file
+ */
+class SqLite3TileCache extends TileCache
+{
+}
