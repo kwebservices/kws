95a3edd27a46a50a859e3c20eb9d87fd6e041869
KRealm Web Services <webmaster@krealmwebservices.com.au>
2020-06-15 14:17:05 +1000

    Added various properties to the joomla module helper class

--- src/Joomla/Module/ModuleHelper.php
+++ src/Joomla/Module/ModuleHelper.php
@@ -10,10 +10,121 @@
 
 namespace KWS\Joomla\Module;
 
+use \Joomla\Registry\Registry;
+
 
 /**
  * Helper/utility class for working with module extensions
  */
 class ModuleHelper
 {
+
+    /**
+     * ID of the module in the site's database
+     *
+     * @var int
+     */
+    public $id = 0;
+
+
+    /**
+     * Title given to the module
+     *
+     * @var string
+     */
+    public $title = '';
+
+
+    /**
+     * Name of the module (with mod_)
+     *
+     * @var string
+     */
+    public $module = '';
+
+    /**
+     * Position the module will be displayed at
+     *
+     * @var string
+     */
+    public $position = '';
+
+
+    /**
+     * Show/hide the module's title
+     *
+     * @var bool
+     */
+    public $showtitle = false;
+
+
+    /**
+     * Contains the bulk of the modules params/config
+     *
+     * @var \Joomla\Registry\Registry
+     */
+    public $params = null;
+
+
+    /**
+     * Name of the module (without mod_)
+     *
+     * @var string
+     */
+    public $name = '';
+
+
+    /**
+     * Module layout to be displayed
+     *
+     * @var string
+     */
+    public $layout = 'default';
+
+    /**
+     * HTML tag to use for the module container
+     *
+     * @var string
+     */
+    public $tag = 'div';
+
+
+    /**
+     * CSS ID for the module's container element
+     *
+     * @var string
+     */
+    public $cssId = '';
+
+
+    /**
+     * CSS class for the module's container element
+     *
+     * @var string
+     */
+    public $cssClass = '';
+
+
+
+    /**
+     * Initialises new instances of this class
+     * -------------------------------------------------------------------------
+     * @param mixed     $module     Module data
+     */
+    public function __construct($module)
+    {
+        // Initialise some class variables
+        $this->id        = $module->id;
+        $this->title     = $module->title;
+        $this->module    = $module->module;
+        $this->position  = $module->position;
+        $this->showtitle = $module->showtitle;
+        $this->params    = new Registry($module->params);
+        $this->name      = $module->name;
+        $this->layout    = $this->params->get('layout', 'default');
+        $this->tag       = $this->params->get('module_tag', 'div');
+        $this->cssId     = "module-{$this->id}";
+        $this->cssClass  = "{$this->module} {$this->module}_{$this->layout}";
+    }
+
 }
