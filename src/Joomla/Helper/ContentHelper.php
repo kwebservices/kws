9aac8e762f0657a074df2c800494206b33d477ca
David Plath <support@krealmwebservices.com.au>
2020-07-01 17:03:26 +1000

    Added helper/utility class for working with joomla articles and categories

---
+++ src/Joomla/Helper/ContentHelper.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * =============================================================================
+ * @package     KRealm Web Services PHP Library
+ * @author      David Plath <webmaster@krealmwebservices.com.au>
+ * @copyright   Copyright (C) 2020 KRealm Web Services. All rights reserved.
+ * @license     GNU General Public License version 3 or later
+ * =============================================================================
+ */
+
+namespace KWS\Joomla\Helper;
+
+use \Joomla\CMS\Categories\Categories;
+
+
+/**
+ * Helper/utility class for working with joomla articles and categories
+ */
+class ContentHelper
+{
+
+    /**
+     * Get a joomla category by Id
+     * -------------------------------------------------------------------------
+     * @param  int    $id   Id of the Joomla category
+     *
+     * @return mixed
+     */
+    public static function getCategoryById(int $id)
+    {
+        $result = Categories::getInstance('content')->get($id);
+        $result->params = json_decode($category->params);
+        return $result;
+    }
+
+
+    /**
+     * Get a joomla article by Id
+     * -------------------------------------------------------------------------
+     * @param  int    $id   Id of the Joomla article
+     *
+     * @return mixed
+     */
+    public static function getArticleById(int $id)
+    {
+        $model = ComponentHelper::getModel('com_content', 'Article');
+        $result = $model->getItem($id);
+        return $result;
+    }
+
+}
