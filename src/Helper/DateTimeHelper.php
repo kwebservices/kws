8a8e5e5cba89dc925ab4be0de9857193b2d1a5d0
KRealm Web Services <webmaster@krealmwebservices.com.au>
2020-08-16 22:10:35 +1000

    Added helper class for working with dates and/or time

---
+++ src/Helper/DateTimeHelper.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * =============================================================================
+ * @package     KRealm Web Services PHP Library
+ * @author      David Plath <webmaster@krealmwebservices.com.au>
+ * @copyright   Copyright (C) 2020 KRealm Web Services. All rights reserved.
+ * @license     GNU General Public License version 3 or later
+ * =============================================================================
+ */
+
+namespace KWS\Helper;
+
+
+/**
+ * Helper class for working with dates and/or time
+ */
+class DateTimeHelper extends Helper
+{
+
+    /**
+     * Convert a given number of seconds into a hour:minute:seconds string
+     * -------------------------------------------------------------------------
+     * @param  int      $seconds    The value to convert
+     * @param  string   $separator  Charictar/string to seperate the hr,min,sec.
+     *
+     * @return string   A string containing hours, minutes and seconds
+     */
+    public static function secondsToHMS($seconds, $separator = ':')
+    {
+        // Compose the result
+        $hrs    = str_pad(floor($seconds/3600), 2, '0', STR_PAD_LEFT);
+        $min    = str_pad(($seconds/60) % 60, 2, '0', STR_PAD_LEFT);
+        $sec    = str_pad($seconds % 60, 2, '0', STR_PAD_LEFT);
+        $result = $hrs . $separator . $min . $separator . $sec;
+
+        // Return the result
+        return $result;
+    }
+
+
+
+}
--- src/Utils.php
+++ src/Utils.php
@@ -137,25 +137,7 @@ class Utils
     }
 
 
-    /**
-     * Convert a given number of seconds into a hour:minute:seconds string
-     * -------------------------------------------------------------------------
-     * @param  int      $seconds    The value to convert
-     * @param  string   $separator  Charictar/string to seperate the hr,min,sec.
-     *
-     * @return string   A string containing hours, minutes and seconds
-     */
-    public static function secondsToHMS($seconds, $separator = ':')
-    {
-        // Compose the result
-        $hrs    = str_pad(floor($seconds/3600), 2, '0', STR_PAD_LEFT);
-        $min    = str_pad(($seconds/60) % 60, 2, '0', STR_PAD_LEFT);
-        $sec    = str_pad($seconds % 60, 2, '0', STR_PAD_LEFT);
-        $result = $hrs . $separator . $min . $separator . $sec;
-
-        // Return the result
-        return $result;
-    }
+    
 
 
 
