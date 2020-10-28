ee33c79999a66b55732a660a12f6e4106d5e40e2
KRealm Web Services <webmaster@krealmwebservices.com.au>
2020-10-28 12:07:46 +1000

    Added construtor and initialise() method to the joomla template helper

--- src/Joomla/Template/TemplateHelper.php
+++ src/Joomla/Template/TemplateHelper.php
@@ -10,10 +10,79 @@
 
 namespace KWS\Joomla\Template;
 
+use \Joomla\CMS\Factory;
+use \KWS\Joomla\Document\DocumentHelper;
+
 
 /**
  * Helper/utility class for working with template extensions
  */
 class TemplateHelper
 {
+
+    /**
+     * The current template
+     *
+     * @var \Joomla\Registry\Registry
+     */
+    protected $template = null;
+
+
+    /**
+     * Global document object
+     *
+     * @var \Joomla\CMS\Document\Document
+     */
+    protected $document = null;
+
+
+    /**
+     * Absolute path to the template's folder
+     * -------------------------------------------------------------------------
+     * @var string
+     */
+    public $basePath = '';
+
+
+    /**
+     * Base URL to the template's folder
+     * -------------------------------------------------------------------------
+     * @var string
+     */
+    public $baseURL = '';
+
+
+
+    /**
+     * Constructor for initialising new instances of this class
+     * -------------------------------------------------------------------------
+     * @return void
+     */
+    public function __construct()
+    {
+        // Init some class variables
+        $this->template = Factory::getApplication()->getTemplate(true);
+        $this->document = Factory::getDocument();
+        $this->basePath = JPATH_THEMES . DIRECTORY_SEPARATOR . $this->template->template;
+        $this->baseUrl  = Factory::getUri()->base();
+    }
+
+
+    /**
+     * Initialise the helper/template
+     * -------------------------------------------------------------------------
+     * @return void
+     */
+    public function initialise() : void
+    {
+        // Set the document type to HTML5
+        $this->document->setHtml5(true);
+
+        // Remove the generator meta tag (if it exists)
+        $this->document->setGenerator('');
+
+        // Use the page title (without the site name) as the meta title
+        $this->document->setMetaData('title', DocumentHelper::getTitle());
+    }
+
 }
