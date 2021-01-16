6875f677fb19cd8fab4a077157a657a72c4f0cf3
KRealm Web Services <webmaster@krealmwebservices.com.au>
2021-01-17 03:58:29 +1000

    Added generic model for CodeIgniter

---
+++ src/CodeIgniter/V4/Model/Model.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * =============================================================================
+ * @package     KRealm Web Services PHP Library
+ * @author      David Plath <webmaster@krealmwebservices.com.au>
+ * @copyright   Copyright (C) 2020 KRealm Web Services. All rights reserved.
+ * @license     GNU General Public License version 3 or later
+ * =============================================================================
+ */
+
+namespace KWS\CodeIgniter\V4\Model;
+
+use \Config\Database;
+
+
+/**
+ * Generic CodeIgniter Model
+ */
+class Model
+{
+    
+    /**
+     * Database connection
+     *
+     * @var CodeIgniter\Database\ConnectionInterface
+     */
+    protected $db;
+
+        
+    /**
+     * Construtor for initialising new instances of this class
+     * -------------------------------------------------------------------------
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->db = Database::connect();
+    }
+
+}
