f51ff9416359e6fed5c7242214c9a79577d2c162
KRealm Web Services <webmaster@krealmwebservices.com.au>
2021-02-25 16:56:17 +1000

    Updated copyright year to 2021

--- src/Joomla/Model/Administrator/ItemModel.php
+++ src/Joomla/Model/Administrator/ItemModel.php
@@ -3,7 +3,7 @@
  * =============================================================================
  * @package     KRealm Web Services PHP Library
  * @author      David Plath <webmaster@krealmwebservices.com.au>
- * @copyright   Copyright (C) 2020 KRealm Web Services. All rights reserved.
+ * @copyright   Copyright (C) 2021 KRealm Web Services. All rights reserved.
  * @license     GNU General Public License version 3 or later
  * =============================================================================
  */
