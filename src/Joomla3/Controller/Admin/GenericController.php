8728e0fbf99e029394b82999218ecd4837a62644
KRealm Web Services <webmaster@krealmwebservices.com.au>
2021-02-25 15:32:05 +1000

    Added various Joomla 3.x admin controllers

---
+++ src/Joomla3/Controller/Admin/GenericController.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * =============================================================================
+ * @package     KRealm Web Services PHP Library
+ * @author      David Plath <webmaster@krealmwebservices.com.au>
+ * @copyright   Copyright (C) 2020 KRealm Web Services. All rights reserved.
+ * @license     GNU General Public License version 3 or later
+ * =============================================================================
+ */
+
+namespace KWS\Joomla3\Controller\Admin;
+
+use Joomla\CMS\MVC\Controller\BaseController;
+
+
+/**
+ * Base class for creating generic back-end controllers
+ */
+class GenericController extends BaseController
+{
+}
+
---
+++ src/Joomla3/Controller/Admin/ListController.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * =============================================================================
+ * @package     KRealm Web Services PHP Library
+ * @author      David Plath <webmaster@krealmwebservices.com.au>
+ * @copyright   Copyright (C) 2020 KRealm Web Services. All rights reserved.
+ * @license     GNU General Public License version 3 or later
+ * =============================================================================
+ */
+
+namespace KWS\Joomla3\Controller\Admin;
+
+use Joomla\CMS\MVC\Controller\AdminController;
+
+
+/**
+ * Base class for creating list based back-end controllers
+ */
+class ListController extends AdminController
+{
+}
