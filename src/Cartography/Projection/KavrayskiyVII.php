318f49c27b8e34734ce9984fe2d785e51125143a
KRealm Web Services <webmaster@krealmwebservices.com.au>
2021-02-25 16:57:44 +1000

    Changed author email address to admin@krealmwebservices.com.au

--- src/Cartography/Projection/KavrayskiyVII.php
+++ src/Cartography/Projection/KavrayskiyVII.php
@@ -2,7 +2,7 @@
 /**
  * =============================================================================
  * @package     KRealm Web Services PHP Library
- * @author      David Plath <webmaster@krealmwebservices.com.au>
+ * @author      David Plath <admin@krealmwebservices.com.au>
  * @copyright   Copyright (C) 2021 KRealm Web Services. All rights reserved.
  * @license     GNU General Public License version 3 or later
  * =============================================================================
