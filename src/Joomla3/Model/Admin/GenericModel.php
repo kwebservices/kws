be80890cc5c6702c34678a19fc7613f305642dfd
KRealm Web Services <webmaster@krealmwebservices.com.au>
2021-02-25 15:50:08 +1000

    Added various Joomla 3.x admin models

---
+++ src/Joomla3/Model/Admin/GenericModel.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * =============================================================================
+ * @package     KRealm Web Services PHP Library
+ * @author      David Plath <webmaster@krealmwebservices.com.au>
+ * @copyright   Copyright (C) 2020 KRealm Web Services. All rights reserved.
+ * @license     GNU General Public License version 3 or later
+ * =============================================================================
+ */
+
+namespace KWS\Joomla3\Model\Admin;
+
+use \Joomla\CMS\MVC\Model\BaseDatabaseModel;
+
+
+/**
+ * Base class for creating generic back-end models
+ */
+class GenericModel extends BaseDatabaseModel
+{
+}
---
+++ src/Joomla3/Model/Admin/ListModel.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * =============================================================================
+ * @package     KRealm Web Services PHP Library
+ * @author      David Plath <webmaster@krealmwebservices.com.au>
+ * @copyright   Copyright (C) 2020 KRealm Web Services. All rights reserved.
+ * @license     GNU General Public License version 3 or later
+ * =============================================================================
+ */
+
+namespace KWS\Joomla3\Model\Admin;
+
+use \Joomla\CMS\MVC\Model\ListModel AS JListModel;
+
+
+/**
+ * Base class for creating list based back-end models
+ */
+class ListModel extends JListModel
+{
+
+    /**
+     * Get a JDatabaseQuery object for retrieving the dataset from database
+     * -------------------------------------------------------------------------
+     * @return \JDatabaseQuery  
+     */
+    protected function getListQuery()
+    {
+        // Initialise some local variables        
+        $database = $this->getDbo();
+        $query    = parent::getListQuery();
+
+        // Add the order by clause
+        $ordering   = $database->escape($this->getState('list.ordering'));
+        $direction  = $database->escape($this->getState('list.direction'));
+        if (!empty($ordering) and !empty($direction))
+            $query->order($ordering . ' ' . $direction);
+
+        // Return the result
+        return $query;
+    }
+
+
+    /**
+     * Get the current ordering column
+     * -------------------------------------------------------------------------
+     * @return string
+     */
+    public function getOrdering()
+    {
+        return $this->state->get('list.ordering');
+    }
+
+
+    /**
+     * Get the current ordering direction
+     * -------------------------------------------------------------------------
+     * @return string
+     */
+    public function getDirection()
+    {
+        return $this->state->get('list.direction');
+    }    
+
+}
